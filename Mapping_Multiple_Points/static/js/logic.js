0fafa4b7a5d01de89fb8f1669cc92df303558845
Jacob Wade <jacoblorenwade@Jacobs-MacBook-Pro.local>
2020-06-03 19:46:22 -0700

    Starting multiple points branch

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,31 @@
+// Add console.log to confirm that the code is working
+console.log("working");
+
+// Create the map object with a ceter and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Create a marker
+let marker = L.circle([34.0522, -118.2437], {
+	radius: 300,
+	color: 'black',
+    fillColor: 'yellow',
+ }).addTo(map);
+
+// Alternate way of creating map (useful when adding multiple tile layers
+// or background image of maps)
+// let map = L.map("mapid", {
+//     center: [
+//         40.7, -94.5
+//     ],
+//     zoom: 4
+// });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
