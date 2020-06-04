087508cda7d4da6a1d9584b9031e282162bc8386
Jacob Wade <jacoblorenwade@Jacobs-MacBook-Pro.local>
2020-06-03 20:37:04 -0700

    Adding code and cities file

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,13 +4,6 @@ console.log("working");
 // Create the map object with a ceter and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Create a marker
-let marker = L.circle([34.0522, -118.2437], {
-	radius: 300,
-	color: 'black',
-    fillColor: 'yellow',
- }).addTo(map);
-
 // Alternate way of creating map (useful when adding multiple tile layers
 // or background image of maps)
 // let map = L.map("mapid", {
@@ -19,6 +12,32 @@ let marker = L.circle([34.0522, -118.2437], {
 //     ],
 //     zoom: 4
 // });
+
+// Create a marker
+// let marker = L.circle([34.0522, -118.2437], {
+// 	radius: 300,
+// 	color: 'black',
+//     fillColor: 'yellow',
+//  }).addTo(map);
+
+// Create a cities array
+// Moved this to cities.js
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create a marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+		radius: city.population/100000,
+		color: 'orange',
+		fillColor: 'orange',
+		weight: 4
+	})
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+	.addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
